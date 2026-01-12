218e45e84fa71559bced73bd7482d71c828a615c
imtiazhossain007 <imtiazhossain9734@gmail.com>
2026-01-13 01:47:54 +0530

    api adjusted

--- mobile-app/services/api.ts
+++ mobile-app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://junita-blondish-blanca.ngrok-free.dev/";
+const API_BASE_URL = "https://taskmanagement-app-1-otnq.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
