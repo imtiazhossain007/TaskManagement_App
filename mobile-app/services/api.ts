f7a4c1a89897803985785db4cf08566496176574
imtiazhossain007 <imtiazhossain9734@gmail.com>
2026-01-13 00:56:26 +0530

    api adjusted

--- mobile-app/services/api.ts
+++ mobile-app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.1.33:5000/api";
+const API_BASE_URL = "https://junita-blondish-blanca.ngrok-free.dev/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
